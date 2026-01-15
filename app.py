5a2033aa22c6e1eba98203e3fb30dee84fb4aa35
zion tecnologia <alexcostasilva2810@gmail.com>
2026-01-15 07:41:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,18 @@ import requests
 # --- CONFIGURAÇÃO PARA ÍCONE E APP INSTALÁVEL (PWA) ---
 st.markdown(f"""
     <head>
-        <link rel="manifest" href="./manifest.json">
+        <link rel="manifest" href="https://raw.githubusercontent.com/alexcostasilva2810-create/Rancho-Zion/main/manifest.json?v=2">
+        <meta name="mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-        <link rel="apple-touch-icon" href="./logo_pwa.png">
+        <link rel="icon" type="image/png" sizes="512x512" href="https://raw.githubusercontent.com/alexcostasilva2810-create/Rancho-Zion/main/logo_pwa.png">
+        <link rel="apple-touch-icon" sizes="512x512" href="https://raw.githubusercontent.com/alexcostasilva2810-create/Rancho-Zion/main/logo_pwa.png">
     </head>
-    """, unsafe_allow_html=True)
-
-# CSS PARA ESCONDER O MENU DO STREAMLIT E DEIXAR O APP LIMPO
-st.markdown("""
     <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    .block-container {padding-top: 1rem;}
+        #MainMenu {{visibility: hidden;}}
+        footer {{visibility: hidden;}}
+        header {{visibility: hidden;}}
+        .block-container {{padding-top: 0rem;}}
     </style>
     """, unsafe_allow_html=True)
 # -----------------------------------------------------
