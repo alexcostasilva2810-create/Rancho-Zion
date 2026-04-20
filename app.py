d7d322fe8c3affc4cf9e99c631a8512e717fc48d
zion tecnologia <alexcostasilva2810@gmail.com>
2026-04-20 16:17:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ USUARIOS = {
     "CUMARU": {"nome": "JULIO CESA", "senha": "8551"},
     "LUIZ FELIPE": {"nome": "ELIOMA", "senha": "8929"},
     "AROEIRA": {"nome": "ELZA", "senha": "5881"},
-    "ANGICO": {"nome": "JOÃO CARLOS", "senha": "6678"},
+    "ANGICO": {"nome": "SARA ANACLETO", "senha": "6678"},
     "BRENO": {"nome": "DANTAS", "senha": "2870"},
     "SAMAUMA": {"nome": "DANTAS MORAES", "senha": "7211"},
     "ENCARREGADO MANAUS EUCLIDES": {"nome": "Elcicley Dourado", "senha": "301003"},
