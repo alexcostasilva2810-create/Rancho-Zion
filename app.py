7c005b96f4bbffe3623566f38012a4f696e7c002
zion tecnologia <alexcostasilva2810@gmail.com>
2026-06-16 10:56:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ USUARIOS = {
     "SUPERVISOR SANTARÉM": {"nome": "Rafael Artur", "senha": "103010"},
     "IPE": {"nome": "JOHNNATAN", "senha": "8419"},
     "TIMBORANA": {"nome": "ROGILEIA", "senha": "6300"},
-    "CASTANHEIRA": {"nome": "ELEONILDE", "senha": "6300"},
+    "CASTANHEIRA": {"nome": "STEFANI", "senha": "6300"},
     "CANJERANA": {"nome": "ALLAN", "senha": "6310"},
     "QUARUBA": {"nome": "RODRIGO MORANGUINHO", "senha": "6310"}
 }
